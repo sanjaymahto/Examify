ad36b88845e692ccdde3da0e7ecf336948cf2667
sanjaymahto <mahto.sanjay41@gmail.com>
2017-07-22 14:13:58 +0530

    Initial commit

--- public/angular/services/queryService.js
+++ public/angular/services/queryService.js
@@ -2,81 +2,79 @@ myApp.factory('queryService', function queryFactory($http, authService, $q) {
 
     var queryArray = {};
 
-    var baseUrl = "http://localhost:3000";
-
     queryArray.log = '';
     queryArray.sign = '';
 
     //sign up request
     queryArray.signUp = function (userData) {
-        return $http.post(baseUrl + '/users/signup', userData);
+        return $http.post('/users/signup', userData);
     }
 
     //login request
     queryArray.login = function (loginData) {
-        return $http.post(baseUrl + '/users/login', loginData);
+        return $http.post('/users/login', loginData);
     }
 
     //login request for facebook
     queryArray.loginFacebook = function () {
-        return $http.get(baseUrl + '/auth/facebook');
+        return $http.get('/auth/facebook');
         //window.location.href="/auth/facebook";
     }
 
     //login request for google
     queryArray.loginGoogle = function () {
-        return $http.get(baseUrl + '/auth/google');
+        return $http.get('/auth/google');
         //window.location.href="/auth/google";
     }
 
     //login request for reset password
     queryArray.resetpass = function (data) {
-        return $http.post(baseUrl + '/users/reset', data);
+        return $http.post('/users/reset', data);
     }
 
     //login request to get security Question
     queryArray.getquestion = function (data) {
-        return $http.post(baseUrl + '/users/securityQuestion', data);
+        return $http.post('/users/securityQuestion', data);
     }
 
     //login request to set the new Password
     queryArray.resetpassword = function (data) {
-        return $http.post(baseUrl + '/users/resetpassword', data);
+        return $http.post('/users/resetpassword', data);
     }
 
     // request to Create a test by admin
     queryArray.createTest = function (data) {
-        return $http.post(baseUrl + '/tests/createTest/admin?token=' + authService.getToken(), data);
+        return $http.post('/tests/createTest/admin?token=' + authService.getToken(), data);
     }
 
     // request to Create a Question by Admin
     queryArray.getTests = function (data) {
-        return $http.get(baseUrl + '/tests/allTests?token=' + authService.getToken(), data);
+        return $http.get('/tests/allTests?token=' + authService.getToken(), data);
     }
 
     // request to Create a Question by Admin
     queryArray.createQuestion = function (data) {
         //console.log(data.id);
-        return $http.post(baseUrl + '/tests/' + data.id + '/createquestions?token=' + authService.getToken(), data);
+        return $http.post('/tests/' + data.id + '/createquestions?token=' + authService.getToken(), data);
     }
 
 
     // request to delete a Test by Admin
     queryArray.deleteTest = function (data) {
        // console.log(data);
-        return $http.delete(baseUrl + '/tests/test/delete/' + data + '?token=' + authService.getToken(), data);
+        return $http.delete('/tests/test/delete/' + data + '?token=' + authService.getToken(), data);
     }
 
     // request to view Questions by Admin
     queryArray.viewQuestion = function (data) {
        // console.log(data);
-        return $http.get(baseUrl + '/tests/' + data + '/getquestions?token=' + authService.getToken(), data);
+        return $http.get('/tests/' + data + '/getquestions?token=' + authService.getToken(), data);
     }
 
     // request to Update test by Admin
     queryArray.UpdateTest = function (data) {
         //console.log(data.id);
-        return $http.put(baseUrl + '/tests/test/update/' + data.id + '?token=' + authService.getToken(), data);
+        return $http.put('/tests/test/update/' + data.id + '?token=' + authService.getToken(), data);
     }
 
     // request to Update test by Admin
@@ -84,7 +82,7 @@ myApp.factory('queryService', function queryFactory($http, authService, $q) {
         //console.log(data);
         //console.log(index);
         //console.log(quesid);
-        return $http.get(baseUrl + '/tests/question/' + data.id + '/' + index + '/' + quesid + '/delete?token=' + authService.getToken());
+        return $http.get('/tests/question/' + data.id + '/' + index + '/' + quesid + '/delete?token=' + authService.getToken());
     }
 
     // request to Update test by Admin
@@ -92,49 +90,49 @@ myApp.factory('queryService', function queryFactory($http, authService, $q) {
        // console.log(data);
        // console.log(index);
        // console.log(quesid);
-        return $http.post(baseUrl + '/tests/question/' + data.id + '/' + index + '/' + quesid + '/update?token=' + authService.getToken(), data);
+        return $http.post('/tests/question/' + data.id + '/' + index + '/' + quesid + '/update?token=' + authService.getToken(), data);
     }
 
     // request to User Info
     queryArray.userInfo = function () {
-        return $http.get(baseUrl + '/users/getUserinfo?token=' + authService.getToken());
+        return $http.get('/users/getUserinfo?token=' + authService.getToken());
     }
 
     // request to User Info Facebook
     queryArray.userInfofacebook = function () {
-        return $http.get(baseUrl + '/getUserinfofacebook?token=' + authService.getToken());
+        return $http.get('/getUserinfofacebook?token=' + authService.getToken());
     }
 
     // request to User Info Facebook
     queryArray.userInfogoogle = function () {
-        return $http.get(baseUrl + '/getUserinfogoogle?token=' + authService.getToken());
+        return $http.get('/getUserinfogoogle?token=' + authService.getToken());
     }
 
     //Query to get questions for single test
     queryArray.getSingleTest = function (singleTestId) {
         //console.log(singleTestId);
-        return $http.get(baseUrl + '/tests/test/' + singleTestId + '?token=' + authService.getToken());
+        return $http.get('/tests/test/' + singleTestId + '?token=' + authService.getToken());
     }
 
     //Query to get questions for single test
     queryArray.submitAnswer = function (data) {
-        return $http.post(baseUrl + '/tests/tests/' + data.testid + '/questions/' + data.questionid + '/answer?token=' + authService.getToken(), data);
+        return $http.post('/tests/tests/' + data.testid + '/questions/' + data.questionid + '/answer?token=' + authService.getToken(), data);
     }
 
 
     //Query to get questions for single test
     queryArray.submitTest = function (data) {
-        return $http.post(baseUrl + '/tests/performance/' + data.testid + '?token=' + authService.getToken(), data);
+        return $http.post('/tests/performance/' + data.testid + '?token=' + authService.getToken(), data);
     }
 
     //Query to post test attempted by
     queryArray.testAttempted = function (attempdata) {
-        return $http.put(baseUrl + '/tests/tests/' + attempdata.testId + '/attemptedby?token=' + authService.getToken(), attempdata);
+        return $http.put('/tests/tests/' + attempdata.testId + '/attemptedby?token=' + authService.getToken(), attempdata);
     }
 
     //Query to get performance of users in dashboard
     queryArray.getusertestdetails = function (userid) {
-        return $http.get(baseUrl + '/tests/performance/user/' + userid + '?token=' + authService.getToken());
+        return $http.get('/tests/performance/user/' + userid + '?token=' + authService.getToken());
     }
 
 
